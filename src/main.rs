70e64f7c523a5a5027bdb9a2aa4278ae14cf5f91
Benjamin Delmas <bdelmas.pro@gmail.com>
2018-08-19 23:36:29 +0200

    Add code to load and save an image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate image;
+
+use image::GenericImage;
+
 fn main() {
-    println!("Hello, world!");
+    // Use the open function to load an image from a Path.
+    // ```open``` returns a `DynamicImage` on success.
+    let img = image::open("test.jpg").unwrap();
+
+    // The dimensions method returns the images width and height.
+    println!("dimensions {:?}", img.dimensions());
+
+    // The color method returns the image's `ColorType`.
+    println!("{:?}", img.color());
+
+    img.save("test.png").unwrap();
 }
